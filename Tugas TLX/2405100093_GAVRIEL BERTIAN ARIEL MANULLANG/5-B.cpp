6d9172cf4b4bbe4e0b0d25cc9dbcc15bfc169a34
gavrielariel <gavrielariel325@gmail.com>
2024-10-10 23:34:35 +0700

    Update 5-B.cpp

---
+++ Tugas TLX/2405100093_GAVRIEL BERTIAN ARIEL MANULLANG/5-B.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // Menyertakan library input-output standar C++
+using namespace std; // Memungkinkan penggunaan elemen dari namespace std tanpa prefiks 'std::'
+
+int main() { // Fungsi utama tempat eksekusi program dimulai
+    bool ada_yang_dijual = false; // Variabel untuk menandai jika ada yang dijual, diinisialisasi dengan false
+
+    for (int sisi = 121; sisi <= 125; sisi++) { // Loop dari 121 hingga 125
+        bool kelipatan_2 = sisi % 2 == 0; // Memeriksa apakah 'sisi' adalah kelipatan 2
+        bool kelipatan_3 = sisi % 3 == 0; // Memeriksa apakah 'sisi' adalah kelipatan 3
+
+        if (kelipatan_2 && !kelipatan_3) { // Jika 'sisi' kelipatan 2 dan bukan kelipatan 3
+            ada_yang_dijual = true; // Set variabel 'ada_yang_dijual' menjadi true
+        }
+    }
+
+    if (ada_yang_dijual) { // Memeriksa nilai 'ada_yang_dijual'
+        cout << "ada" << endl; // Jika true, cetak "ada"
+    } else {
+        cout << "tidak ada" << endl; // Jika false, cetak "tidak ada"
+    }
+}
